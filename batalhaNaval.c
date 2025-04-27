433f232037e3307ef01584af59385cd02641000b
John <139019965+JBigode@users.noreply.github.com>
2025-04-27 12:05:14 +0000

    Aventureiro concluido

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 
-int main (){
-    char linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'j'};
-    //1. Criando o Tabuleiro (Matriz 10x10)
-    int tabuleiro [10][10];
+#define LINHAS 10
+#define COLUNAS 10
 
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++){
+int main (){
+
+    //1. Criando o tabuleiro (Matriz 10x10)
+    int tabuleiro [LINHAS][COLUNAS];
+
+    for (int i = 0; i < LINHAS; i++){
+        for (int j = 0; j < COLUNAS; j++){
             tabuleiro[i][j] =0;
         }
     }
 
     // Posicionar navio em F7 (vertical) 
-    for (int i = 6; i < 9; i++) { 
+    for (int i = 6; i < 9; i++) { // ocupar 3 casas pra baixo
         tabuleiro[i][5] = 3; // 3 representa navio
     }
 
     // Posicionar navio em C3 (horizontal) 
-    for (int j = 2; j < 5; j++) { // ocupar 3 casas pra direita
-        tabuleiro[2][j] = 3; // 3 representa navio
+    for (int j = 3; j < 6; j++) { // ocupar 3 casas pra direita
+        tabuleiro[3][j] = 3; // 3 representa navio
+    }
+
+    // Posicionar navio na diagonal principal 
+    for (int i = 0; i <= 2; i++) {
+        tabuleiro[i][i] = 3;
+    }
+
+    // Posicionar navio na diagonal secundária 
+    for (int i = 0; i <= 2; i++) {
+        tabuleiro[i][9 - i] = 3;
     }
     
-    // Imprimindo o titulo
-    printf("TABULEIRO BATALHA NAVAL\n\n ");
-    for (int i = 0; i < 10; i++)
+    // Imprimindo o tabuleiro
+    printf("TABULEIRO BATALHA NAVAL\n\n");
+    printf("   ");
+    for (char letra = 'A'; letra <= 'J'; letra++)
     {
-        printf(" %c", linha[i]);
+        printf("%c ", letra);
     }
     printf("\n");
 
